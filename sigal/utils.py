fff2d7a9090aa7d4c29b8bd8dca163225ca9fcc5
Stéphane Gourichon <stephane@gourichon.org>
2020-06-17 18:29:13 +0200

    Fix bug when symlink and not rellink.

--- sigal/utils.py
+++ sigal/utils.py
@@ -47,7 +47,7 @@ def copy(src, dst, symlink=False, rellink=False):
     func = os.symlink if symlink else shutil.copy2
     if symlink and os.path.lexists(dst):
         os.remove(dst)
-    if rellink:  # relative symlink from dst
+    if symlink and rellink:  # relative symlink from dst
         func(os.path.relpath(src, os.path.dirname(dst)), dst)
     else:
         func(src, dst)
